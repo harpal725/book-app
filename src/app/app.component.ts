ec45414f1ee9d0a765c86cdbaf4c290a8eff825b
Harpal Singh <Harpal Singh@DESKTOP-JG9O5VC>
2025-07-26 19:37:28 +0500

    2nd day

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { CommonModule } from '@angular/common';
+
+
+ export enum direction {
+  North, South,East,West
+} 
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'book-app';
+
+  name: string[] = [];
+
+  names: readonly string[] = ["hello readonly","new life"]
+
+  turple: [number, string, string]
+
+  //index signature
+  indexsign : {[index : string]: number } = {};
+
+  AllDirectiom = direction
+
+  directionkeys: string[] =[];
+
+  constructor(){
+  this.name.push("hello");
+  this.name.push("new data")
+  this.turple = [1, '2','3'];
+  this.indexsign["harpal"] = 29;
+  this.indexsign["robi"] = 26;
+  this.indexsign["manroop"] = 1.8;
+
+  this.directionkeys = Object.keys(direction).filter(key => isNaN(Number(key)));
+  }
+
 }
---
+++ src/app/models/Book.ts
@@ -0,0 +1,7 @@
+export interface Book{
+    id: number;
+    title: string;
+    auther: string;
+    genre: string;
+    description: string;
+}
