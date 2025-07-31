dac64935e9e0d7a3aa4f44a1b3cc97bd52add6c4
Harpal Singh <Harpal Singh@DESKTOP-JG9O5VC>
2025-08-01 02:22:57 +0500

    addbook

--- src/app/book-service.service.ts
+++ src/app/book-service.service.ts
@@ -10,9 +10,13 @@ export class BookServiceService {
 
   private GetBookApi='https://localhost:7136/api/BookStores';
 
+  private PutBookApi='https://localhost:7136/api/BookStores'
+
   constructor(private http: HttpClient) { }
 
   getbooks(): Observable<Book[]>{
     return this.http.get<Book[]>(this.GetBookApi);
   }
+
+  
 }
--- src/app/components/add-book/add-book.component.ts
+++ src/app/components/add-book/add-book.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { Book } from '../../models/Book';
 
 @Component({
   selector: 'app-add-book',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './add-book.component.html',
   styleUrl: './add-book.component.css'
 })
 export class AddBookComponent {
+
+   book: Book[] =[];
+  
+  submitbookform(form: NgForm){
+
+  }
 
 }
